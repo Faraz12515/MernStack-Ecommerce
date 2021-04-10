74ae58503471831154540f313ff53ed8020d3f39
SYED FARAZ ALI <alifaraz7866@gmail.com>
2021-04-10 20:44:37 +0500

    added product validation

--- controllers/product.js
+++ controllers/product.js
@@ -12,6 +12,20 @@ exports.create = (req, res) => {
     if (err) {
       return res.status(400).json({ error: "Image could not be uploaded" });
     }
+    //check for all fields
+    const { name, description, price, category, quantity, shipping } = fields;
+
+    if (
+      !name ||
+      !description ||
+      !price ||
+      !category ||
+      !quantity ||
+      !shipping
+    ) {
+      return res.status(400).json({ error: "All fields are required" });
+    }
+
     let product = new Product(fields);
 
     // 1Kb = 1000
@@ -19,6 +33,11 @@ exports.create = (req, res) => {
 
     // Image Upload //
     if (files.photo) {
+      if (files.photo.size > 1000000) {
+        return res
+          .status(400)
+          .json({ error: "Image should be less than 1mb in size" });
+      }
       product.photo.data = fs.readFileSync(files.photo.path);
       product.photo.contentType = files.photo.type;
     }
