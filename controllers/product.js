89ba8bf534dea8e18cc1b9cd4f76b7522773a2fc
SYED FARAZ ALI <alifaraz7866@gmail.com>
2021-04-12 08:56:17 +0500

    Send product photo/image

--- controllers/product.js
+++ controllers/product.js
@@ -233,3 +233,13 @@ exports.listBySearch = (req, res) => {
       });
     });
 };
+
+//Product Image / Photo
+
+exports.photo = (req, res, next) => {
+  if (req.product.photo.data) {
+    res.set("Content-Type", req.product.photo.contentType);
+    return res.send(req.product.photo.data);
+  }
+  next();
+};
