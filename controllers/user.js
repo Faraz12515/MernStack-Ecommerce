b93f571d0f1bc4d8c620b66821acf305489ef51e
SYED FARAZ ALI <alifaraz7866@gmail.com>
2021-04-12 09:58:17 +0500

    User read and update

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,6 @@
 const User = require("../models/user");
 
+//Getting User by Id
 exports.userById = (req, res, next, id) => {
   User.findById(id).exec((err, user) => {
     if (err || !user) {
@@ -8,4 +9,30 @@ exports.userById = (req, res, next, id) => {
     req.profile = user;
     next();
   });
+};
+
+// Read User
+exports.read = (req, res) => {
+  req.profile.hashed_password = undefined;
+  req.profile.salt = undefined;
+  return res.json(req.profile);
+};
+
+// Update User
+exports.update = (req, res) => {
+  User.findOneAndUpdate(
+    { _id: req.profile._id },
+    { $set: req.body },
+    { new: true },
+    (err, user) => {
+      if (err) {
+        return res
+          .status(400)
+          .json({ error: "You are not authorized to perform this action" });
+      }
+      user.hashed_password = undefined;
+      user.salt = undefined;
+      res.json(user);
+    }
+  );
 };
