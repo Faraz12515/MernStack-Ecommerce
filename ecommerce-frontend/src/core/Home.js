d63203ab7106198cdf9319d0aa9816574cb8e5ac
SYED FARAZ ALI <alifaraz7866@gmail.com>
2021-04-19 10:49:26 +0500

    Products By Arrival and Sell

--- ecommerce-frontend/src/core/Home.js
+++ ecommerce-frontend/src/core/Home.js
@@ -1,9 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Layout from "./Layout";
+import { getProducts } from "./apiCore";
 
 const Home = () => {
+  const [productsBySell, setProductsBySell] = useState([]);
+  const [productsByArrival, setProductsByArrival] = useState([]);
+  const [error, setError] = useState(false);
+
+  // Load Products By Sell //
+  const loadProductsBySell = () => {
+    getProducts("sold").then((data) => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setProductsBySell(data);
+      }
+    });
+  };
+
+  // Load Products By Arrial //
+  const loadProductsByArrival = () => {
+    getProducts("createdAt").then((data) => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setProductsByArrival(data);
+      }
+    });
+  };
+
+  useEffect(() => {
+    loadProductsBySell();
+    loadProductsByArrival();
+  }, []);
+
   return (
-    <Layout title="Home Page" description="Node React E-Commerce App"></Layout>
+    <Layout title="Home Page" description="Node React E-Commerce App">
+      <h2>ProductsByArrival</h2>
+      {JSON.stringify(productsByArrival)}
+
+      <hr />
+
+      <h2>ProductsBySell</h2>
+      {JSON.stringify(productsBySell)}
+    </Layout>
   );
 };
 
